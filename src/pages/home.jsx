8b026861deef5b4ca6816e29c45eec263b61c7ac
arttturslv <arttturslv@gmail.com>
2023-09-19 23:35:45 -0300

    reinstalei o tailwind

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
     <div className="text-sky-70">
-        <h4>Home</h4>
+        <h4 className='text-3xl font-bold '>Home</h4>
         <p>Essa é minha página inicial.</p>
     </div>
 );
