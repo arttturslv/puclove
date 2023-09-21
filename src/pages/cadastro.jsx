0d2fadb6d909227e7e596d666bec99e4b6e99d5c
ArexDiniz <alexgdiniz18@gmail.com>
2023-09-21 19:55:24 -0300

    Criações do cadastro e seu css;

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import "../StyleCadastro.css";
+
 
 const Cadastro = () => (
-    <div className="">
-        <h4 className="text-amber-300">Cadastro</h4>
-        <p>Essa é minha página de cadastro.</p>
+    <div className="container-cadastro">
+    <div className="container-cadastro">
+      <div className="wrap-cadastro">
+        <form className="cadastro-form">
+          
+          <span className="cadastro-form-title">
+          Cadastre e conheça seu amor
+          </span>
+
+          <div className="wrap-input">
+            <h4>Qual seu nome?</h4>
+            <input className="input" type="name" placeholder="Digite seu nome" />
+          </div>
+
+          <div className="wrap-input">
+            <h4>Digite o e-mail</h4>
+            <input className="input" type="email" placeholder="meuemail@sga.pucminas.br" />
+          </div>
+          
+          <div className="wrap-input">
+            <h4>Digite sua senha</h4>
+            <input className="input" type="password" placeholder="********" />
+          </div>
+          <div className="wrap-input">
+            <h4>Confirme a senha</h4>
+            <input className="input" type="password" placeholder="********" />
+          </div>
+          <div className="wrap-input">
+            <h4>Quando você nasceu</h4>
+            <input className="input" type="date" placeholder="01/01/1900" />
+          </div>
+          <div className="container-login-form-btn">
+            <button className="cadastro-form-btn" >Continuar</button>
+          </div>
+          
+        </form>
+      </div>
     </div>
+  </div>
 );
 
 export default Cadastro;
