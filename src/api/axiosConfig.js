293993fe303522a61cd6d9f642a23bdfbae9da56
arttturslv <arttturslv@gmail.com>
2023-12-21 00:37:50 -0300

    edit: deletando arquivos para atualizar com o outro repositorio

--- src/api/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const token = localStorage.getItem("authToken");
-
-export default axios.create({
-    baseURL:"http://localhost:8080/api/v1",
-    "ngrok-skip-browser-warning": "true"
-});
-
