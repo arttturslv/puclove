99a1e20d5381135687716c757ddaab8e7a3147dd
arttturslv <arttturslv@gmail.com>
2024-06-06 12:52:11 -0300

    refac: added existing routes

--- Codigo/front-end/src/api/axiosConfig.js
+++ Codigo/front-end/src/api/axiosConfig.js
@@ -1,9 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
 const token = localStorage.getItem("authToken");
 
-export default axios.create({
-    baseURL:"http://localhost:8080/api/v1",
+const instance = axios.create({
+    baseURL: "http://localhost:8080/api/v1",
     "ngrok-skip-browser-warning": "true"
 });
 
+const auth = axios.create({
+    baseURL: "http://localhost:8080/auth",
+    "ngrok-skip-browser-warning": "true"
+});
+
+const API = {
+    login: (loginInfo) => auth.post('/login', loginInfo),
+    register: (registerInfo) => auth.post('/register', registerInfo),
+    interests: instance.get('/interests')
+};
+
+export default API;
