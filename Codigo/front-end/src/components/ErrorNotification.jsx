a5bb1ae13550752aab964b6612518e4ffbfd683c
arttturslv <arttturslv@gmail.com>
2024-07-26 10:50:21 -0300

    refac: simplifying error logic

--- Codigo/front-end/src/components/ErrorNotification.jsx
+++ Codigo/front-end/src/components/ErrorNotification.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from 'react';
 import close from '../assets/Icons/close.svg'
-export default function ErrorNotification ({displayNotification, errorType, timer}) {
+export default function ErrorNotification (
+    {error, 
+    setError, 
+    timer}) {
 
-    var message;
-    const [progressTimer, setProgressTimer] = useState(0);
-
-    if(errorType == "server") {
-        message = "Não estamos conseguindo entrar em contato com o servidor, tente mais tarde.";
-    } else if (errorType == "cadastro") {
-        message = "Erro durante o cadastro, tente novamente.";
-    } else {
-        message = "Erro inesperado! Por favor espere um momento e tente novamente."
+    var message = "unknown";
+    var codeStatus = error?.response?.status || error?.code;
+    switch (codeStatus) {
+        case "ERR_NETWORK":
+            message = ("Erro de internet, verifique sua conexão. "+ codeStatus.toString())
+            break;
+        case 403:
+            message = ("Não é possivel entrar, verifique suas informações. (403)")
+            break;
+        case 400:
+            message = ("A solicitação enviada é invalida. (400)")
+            break;
+        default:
+            message = ("Erro desconhecido."+codeStatus.toString())
+            break;
     }
 
     const [counter, setCounter] = useState(0);
@@ -23,8 +32,7 @@ export default function ErrorNotification ({displayNotification, errorType, time
         // Limpa o timeout quando o contador atingir ou exceder 100
         if (counter >= 100) {
             clearTimeout(timeoutId);
-            displayNotification(false);
-
+            setError(null);
         }
 
         // Limpa o timeout quando o componente é desmontado
@@ -35,7 +43,7 @@ export default function ErrorNotification ({displayNotification, errorType, time
    return (
     <div className="absolute rounded-lg bottom-[2vw] max-sm:right-0 right-4 max-w-[700px] max-sm:w-full w-[60vw] py-4 bg-[#AD5E5E] opacity-95 block drop-shadow-lg z-10">
         <div className='gap-4 flex p-2 '>
-            <div className="flex items-center">
+            <div onClick={() => setError(null)} className="flex items-center">
                 <img className='w-14' src={close} alt="icone da notificacao"/>
             </div>
             <div className="flex flex-col flex-wrap justify-center text-[#fff]">
@@ -44,8 +52,8 @@ export default function ErrorNotification ({displayNotification, errorType, time
             </div>
         </div>
         
-        <div class="w-full absolute bottom-0 h-1 bg-[#E2C09B] ">
-            <div class="bg-[#573434] h-1" style={{ width: `${counter}%`}}></div>
+        <div className="w-full absolute bottom-0 h-1 bg-[#E2C09B] ">
+            <div className="bg-[#573434] h-1" style={{ width: `${counter}%`}}></div>
         </div>
     </div>
    )
